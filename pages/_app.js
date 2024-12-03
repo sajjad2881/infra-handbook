07678a7e183df1d0cbb365811a66628a4e4dfb5b
Sajjad Zafar <73673391+sajjad2881@users.noreply.github.com>
2024-12-02 18:38:56 -0800

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/tooltip.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 } 
